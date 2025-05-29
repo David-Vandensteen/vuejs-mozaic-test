eac885291c0f4e80cab3c6e405d14fd96e3f504a
David-Vandensteen <dvandensteen@gmail.com>
2025-05-29 14:55:06 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Test Mozaic Styles</h1>
+    <p>Ce projet est un test pour Mozaic Styles.</p>
+    <MButton label="Default button" />
+  </div>
+</template>
+
+<script>
+import { MButton } from '@mozaic-ds/vue-3';
+// import { MButton } from '@mozaic-ds/vue-3/src/components/button';
+
+
+export default {
+  name: 'App',
+  components: {
+    MButton
+  }
+};
+</script>
---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import mozaicSassConfig from '@mozaic-ds/css-dev-tools/sassConfig';
+import mozaicPluginList from '@mozaic-ds/css-dev-tools/postcssPluginConfig';
+import { includePaths } from '@mozaic-ds/css-dev-tools/sassConfig';
+import path from 'path';
+
+const filteredMozaicPluginList = mozaicPluginList.filter(
+  ({ postcssPlugin }) => postcssPlugin !== 'stylelint'
+);
+
+console.log('filteredMozaicPluginList', filteredMozaicPluginList);
+console.log('includePaths', includePaths);
+
+export default defineConfig({
+  filenameHashing: false,
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: includePaths,
+      },
+    },
+    postcss: {
+      postcssOptions: {
+        plugins: filteredMozaicPluginList,
+      },
+    },
+    loaderOptions: {
+      sass: {
+        sassOptions: mozaicSassConfig,
+      },
+    },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  },
+});
